f3f8b90a1dcdda039879b022f14d5be1b0fe018d
Fernanda Vergara <fernandavmelin@gmail.com>
2020-05-29 15:58:13 -0400

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1>Agrega una tarea a tu lista</h1>
+    <input type="text" v-model="tarea" @keyup.enter="add">
+    <button @click="add">Agregar tarea</button>
+    </div>
+    <hijo :lista_tareas="lista_tareas" @descartar="descartar"></hijo>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hijo from './components/Hijo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Hijo
+  },
+  data() {
+    return {
+       tarea: " ",
+      lista_tareas: []
+    }
+  },
+  methods: {
+    add(){
+      this.lista_tareas.push(this.tarea),
+      this.tarea = ''
+    },
+    descartar(tarea){
+      this.lista_tareas.splice(tarea, 1)
+    }
+  },
 }
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: black;
 }
 </style>
---
+++ src/components/Hijo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="tarea in lista_tareas" :key="tarea">{{tarea}} <button @click="descartar(tarea)">Eliminar tarea</button> </li>
+    </ul>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hijo',
+  props: {
+    lista_tareas: Array,
+    tarea: String,
+  },
+  
+  methods:{
+     descartar(tarea){
+      this.$emit("descartar", tarea)
+    }
+  }
+  
+}
+</script>
+
